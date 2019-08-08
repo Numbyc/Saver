1106c33068b7bb99d594120b8d7fd370a8536a54
yubian <yubian@microsoft.com>
2019-08-08 12:25:51 +0800

    init

---
+++ src/main/java/com/numbyc/saver/SaverApplication.java
@@ -0,0 +1,13 @@
+package com.numbyc.saver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaverApplication.class, args);
+	}
+
+}
